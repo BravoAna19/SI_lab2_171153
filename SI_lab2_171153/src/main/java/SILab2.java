82eae18cb2c013a8bf87e92b501a8d0614db005a
BravoAna19 <63423029+BravoAna19@users.noreply.github.com>
2020-06-09 22:21:02 +0200

    Update SILab2.java

--- SI_lab2_171153/src/main/java/SILab2.java
+++ SI_lab2_171153/src/main/java/SILab2.java
@@ -1,5 +1,27 @@
 package main.java;
 
 public class SILab2 {
-
+	public boolean function (User user, List<String> allUsers) {		//A
+        String specialCharacters="!#$%&'()*+,-./:;<=>?@[]^_`{|}";		//B
+        if (user!=null) {							//C
+            if (user.getUsername()!=null && user.getPassword()!=null) {		//D
+                String password = user.getPassword();				//E
+                String passwordLower = password.toLowerCase();			//F
+                if (!passwordLower.contains(user.getUsername().toLowerCase()) && password.length()>=8) {	//G
+                    boolean digit = false, upper = false, special = false;	//H
+                    for (int i=0;i<password.length();i++) {			//I
+                        if (Character.isDigit(password.charAt(i)))		//J
+                            digit = true;					//K
+                        if (Character.isUpperCase(password.charAt(i)))		//L
+                            upper = true;					//M
+                        if (specialCharacters.contains(String.valueOf(password.charAt(i))))			//N
+                            special = true;					//O
+                    }								
+                    if (digit && upper && special)				//P
+                        return true;						//Q
+                }
+            }
+        }
+        return false;				//R
+    }						//S
 }
